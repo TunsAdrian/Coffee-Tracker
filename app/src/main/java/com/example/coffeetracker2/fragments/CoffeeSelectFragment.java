9425cf72c4f03725aad58b6e4540f4491f4a8943
Tuns Adrian <tunsadrian99@gmail.com>
2020-06-27 02:17:52 +0300

    Added shared prefferences for caffeine in each coffee

--- app/src/main/java/com/example/coffeetracker2/fragments/CoffeeSelectFragment.java
+++ app/src/main/java/com/example/coffeetracker2/fragments/CoffeeSelectFragment.java
@@ -6,8 +6,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +31,9 @@ import java.util.Calendar;
 public class CoffeeSelectFragment extends Fragment implements View.OnClickListener {
 
     private static final String TAG = "CoffeeSelectFragment";
+    private static final String SMALL_COFFEE_PICKER_LAST_SELECTED_POSITION = "SMALL_COFFEE_PICKER_LAST_SELECTED_POSITION";
+    private static final String MEDIUM_COFFEE_PICKER_LAST_SELECTED_POSITION = "MEDIUM_COFFEE_PICKER_LAST_SELECTED_POSITION";
+    private static final String LARGE_COFFEE_PICKER_LAST_SELECTED_POSITION = "LARGE_COFFEE_PICKER_LAST_SELECTED_POSITION";
 
     private NumberPicker smallCoffeePicker;
     private NumberPicker mediumCoffeePicker;
@@ -44,65 +49,34 @@ public class CoffeeSelectFragment extends Fragment implements View.OnClickListen
         View root = inflater.inflate(R.layout.fragment_coffee_select, container, false);
         createNotificationChannel();
         initView(root);
+        initCoffeePicker();
         coffeeViewModel = new ViewModelProvider(this).get(CoffeeRepository.class);
-
-        int minPickerValue = 0;
-        int maxPickerValue = 40;
-        int stepSize = 5;
-
-        String[] numberValues = new String[maxPickerValue - minPickerValue + 1];
-        for (int i = 0; i <= maxPickerValue - minPickerValue; i++) {
-            numberValues[i] = String.valueOf((minPickerValue + i) * stepSize);
-        }
-
-        smallCoffeeBtn.setOnClickListener(this);
-        mediumCoffeeBtn.setOnClickListener(this);
-        largeCoffeeBtn.setOnClickListener(this);
-
-        smallCoffeePicker.setMinValue(0);
-        smallCoffeePicker.setMaxValue(numberValues.length - 1);
-        smallCoffeePicker.setValue(8);
-        smallCoffeePicker.setDisplayedValues(numberValues);
-
-        mediumCoffeePicker.setMinValue(0);
-        mediumCoffeePicker.setMaxValue(numberValues.length - 1);
-        mediumCoffeePicker.setValue(12);
-        mediumCoffeePicker.setDisplayedValues(numberValues);
-
-        largeCoffeePicker.setMinValue(0);
-        largeCoffeePicker.setMaxValue(numberValues.length - 1);
-        largeCoffeePicker.setValue(16);
-        largeCoffeePicker.setDisplayedValues(numberValues);
 
         return root;
     }
 
-    @Override
-    public void onClick(View view) {
-        registerCoffee(view);
-    }
-
     private void registerCoffee(View view) {
 
+        String [] numberValues = mediumCoffeePicker.getDisplayedValues();
         // Separate the cases for each type of coffee
         switch (view.getId()) {
             case R.id.frag_small_coffee:
 
-                Coffee smallCoffee = new Coffee("Small", smallCoffeePicker.getValue(), Calendar.getInstance().getTime(), -1);
+                Coffee smallCoffee = new Coffee("Small", Integer.parseInt(numberValues[smallCoffeePicker.getValue()]), Calendar.getInstance().getTime(), -1);
                 coffeeViewModel.insert(smallCoffee);
 
                 Toast.makeText(getContext(), "Small coffee registered", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.frag_medium_coffee:
 
-                Coffee midCoffee = new Coffee("Medium", mediumCoffeePicker.getValue(), Calendar.getInstance().getTime(), -1);
+                Coffee midCoffee = new Coffee("Medium", Integer.parseInt(numberValues[mediumCoffeePicker.getValue()]), Calendar.getInstance().getTime(), -1);
                 coffeeViewModel.insert(midCoffee);
 
                 Toast.makeText(getContext(), "Medium coffee registered", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.frag_large_coffee:
 
-                Coffee largeCoffee = new Coffee("Large", largeCoffeePicker.getValue(), Calendar.getInstance().getTime(), -1);
+                Coffee largeCoffee = new Coffee("Large", Integer.parseInt(numberValues[largeCoffeePicker.getValue()]), Calendar.getInstance().getTime(), -1);
                 coffeeViewModel.insert(largeCoffee);
 
                 Toast.makeText(getContext(), "Large coffee registered", Toast.LENGTH_SHORT).show();
@@ -146,6 +120,11 @@ public class CoffeeSelectFragment extends Fragment implements View.OnClickListen
         }
     }
 
+    @Override
+    public void onClick(View view) {
+        registerCoffee(view);
+    }
+
     private void initView(View v) {
 
         smallCoffeeBtn = v.findViewById(R.id.frag_small_coffee);
@@ -155,5 +134,90 @@ public class CoffeeSelectFragment extends Fragment implements View.OnClickListen
         smallCoffeePicker = v.findViewById(R.id.frag_small_coffee_picker);
         mediumCoffeePicker = v.findViewById(R.id.frag_medium_coffee_picker);
         largeCoffeePicker = v.findViewById(R.id.frag_large_coffee_picker);
+    }
+
+    private void initCoffeePicker() {
+
+        smallCoffeeBtn.setOnClickListener(this);
+        mediumCoffeeBtn.setOnClickListener(this);
+        largeCoffeeBtn.setOnClickListener(this);
+
+        // Set custom values with step size for each number picker
+        int minPickerValue = 0;
+        int maxPickerValue = 40;
+        int stepSize = 5;
+
+        String[] numberValues = new String[maxPickerValue - minPickerValue + 1];
+        for (int i = 0; i <= maxPickerValue - minPickerValue; i++) {
+            numberValues[i] = String.valueOf((minPickerValue + i) * stepSize);
+        }
+
+        smallCoffeePicker.setMinValue(0);
+        smallCoffeePicker.setMaxValue(numberValues.length - 1);
+        smallCoffeePicker.setDisplayedValues(numberValues);
+
+        mediumCoffeePicker.setMinValue(0);
+        mediumCoffeePicker.setMaxValue(numberValues.length - 1);
+        mediumCoffeePicker.setDisplayedValues(numberValues);
+
+        largeCoffeePicker.setMinValue(0);
+        largeCoffeePicker.setMaxValue(numberValues.length - 1);
+        largeCoffeePicker.setDisplayedValues(numberValues);
+
+        // Save last position of the picker in shared preferences for each picker
+        smallCoffeePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                saveIntToSharedPreferences(getContext(), SMALL_COFFEE_PICKER_LAST_SELECTED_POSITION, i1);
+            }
+        });
+
+        mediumCoffeePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                saveIntToSharedPreferences(getContext(), MEDIUM_COFFEE_PICKER_LAST_SELECTED_POSITION, i1);
+            }
+        });
+
+        largeCoffeePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                saveIntToSharedPreferences(getContext(), LARGE_COFFEE_PICKER_LAST_SELECTED_POSITION, i1);
+            }
+        });
+
+        // Set value of each picker from shared preferences, if exists, or put the default value for each one
+        if (getIntFromSharedPreferences(getContext(), SMALL_COFFEE_PICKER_LAST_SELECTED_POSITION) == -1) {
+            smallCoffeePicker.setValue(8);
+        } else {
+            smallCoffeePicker.setValue(getIntFromSharedPreferences(getContext(), SMALL_COFFEE_PICKER_LAST_SELECTED_POSITION));
+        }
+
+        if (getIntFromSharedPreferences(getContext(), MEDIUM_COFFEE_PICKER_LAST_SELECTED_POSITION) == -1) {
+            mediumCoffeePicker.setValue(12);
+        } else {
+            mediumCoffeePicker.setValue(getIntFromSharedPreferences(getContext(), MEDIUM_COFFEE_PICKER_LAST_SELECTED_POSITION));
+        }
+
+        if (getIntFromSharedPreferences(getContext(), LARGE_COFFEE_PICKER_LAST_SELECTED_POSITION) == -1) {
+            largeCoffeePicker.setValue(16);
+        } else {
+            largeCoffeePicker.setValue(getIntFromSharedPreferences(getContext(), LARGE_COFFEE_PICKER_LAST_SELECTED_POSITION));
+        }
+    }
+
+    private void saveIntToSharedPreferences(Context context, String key, int value) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    private int getIntFromSharedPreferences(Context context, String key) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (sharedPreferences.contains(key)) return sharedPreferences.getInt(key, 0);
+        else return -1;
     }
 }
