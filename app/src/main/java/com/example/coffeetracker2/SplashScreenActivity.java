b59de911dac9ce9d88bdb2bd0d9904689b413f05
Tuns Adrian <tunsadrian99@gmail.com>
2020-06-27 13:07:49 +0300

    Added splash screen and application icon

---
+++ app/src/main/java/com/example/coffeetracker2/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.coffeetracker2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreenActivity.this, SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
